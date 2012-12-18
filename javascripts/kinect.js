23e01f32355c52c10a7605adf32db9bf0cd7ed19
Andre Le <andre@andrele.com>
2012-12-18 11:17:14 -0800

    Now tracks first engaged user with full skeleton. Robust enough to allow other users in background once first user has been detected.

--- javascripts/kinect.js
+++ javascripts/kinect.js
@@ -6,39 +6,31 @@
 // are called by the zig object when those things happen.
 
 
-var radar = {
-  onuserfound: function (user) {
-  },
-  onuserlost: function (user) {
-  },
-  ondataupdate: function (zigdata) {
-      if (zigdata.users[2]){
-      var user = zigdata.users[2];
-      var pos = user.position;
-      var el = user.radarelement;
-      var parentElenment = document.body;
-      var zrange = 4000;
-      var xrange = 4000;
-      var pixelwidth = parentElenment.offsetWidth;
-      var pixelheight = parentElenment.offsetHeight;
-      var heightscale = pixelheight / zrange;
-      var widthscale = pixelwidth / xrange;
-      var jump = Math.floor(player.prev_height - pos[1]);
+var engager = zig.EngageUsersWithSkeleton(1);
 
-        // player.x = ((((pos[0] / xrange) + 0.5) * pixelwidth - (el.offsetWidth / 2)) - 200);
-        var newPosition = ((pos[0]*(pixelwidth/2)/800) + 600);
-        player.distance += Math.floor(Math.abs(newPosition-player.x));
-        player.x = newPosition;
-        player.y = ((game.floor) - (pos[1]).clamp(0, 250)*1.5);
-        console.log('Jump:' + jump);
-        console.log('pos[1]: '  + pos[1]);
-        console.log('Distance: ' + player.distance);
-      };
-  }
-};
+engager.addEventListener('userengaged', function (user) {
+  console.log('User engaged: ' + user.id);
+
+  user.addEventListener('userupdate', function(user) {
+      var pos = user.position;
+      var parentElement = document.body;
+      var pixelwidth = parentElement.offsetWidth;
+      var newPosition = ((pos[0]*(pixelwidth/2)/800) + 600);
+      player.distance += Math.floor(Math.abs(newPosition-player.x));
+      player.x = newPosition;
+      player.y = ((game.floor) - (pos[1]).clamp(0, 250)*1.5);
+      console.log('pos[1]: ' + pos[1]);
+      console.log('pos[2]: ' + pos[2]);
+      console.log('Distance: ' + player.distance);
+  })
+});
+
+engager.addEventListener('userdisengaged', function (user) {
+  console.log('User disengaged: ' + user.id);
+});
 
 // Add the radar object as a listener to the zig object, so that
 // the zig object will call the radar object's callback functions
 
-zig.addListener(radar);
+zig.addListener(engager);
 
